db3555df551ea2cfef99139c12ac83ea65923023
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-08-20 12:32:12 -0700

    commit tree bbfa0d91d9dd5b94aab28843ee2529980bca6a21

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, eventName) {
+    let messages = []
+    for (let i = 0; i < nameArray.length; i++) {
+
+        messages.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
